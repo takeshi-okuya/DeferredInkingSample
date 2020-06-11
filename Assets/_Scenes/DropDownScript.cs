0e7944d83c1964abcc824881e4f2763a238e22c3
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2020-06-12 00:37:39 +0900

    提案法と従来法を生成中に変更可能にした

---
+++ Assets/_Scenes/DropDownScript.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace WCGL
+{
+    public class DropDownScript : MonoBehaviour
+    {
+        Dropdown dropdown;
+
+        Shader deferredInkingShader;
+        Shader backPolygonShader;
+        DeferredInkingModel[] models;
+        Dictionary<string, Shader> originalShaders = new Dictionary<string, Shader>();
+
+        void Start()
+        {
+            dropdown = GetComponent<Dropdown>();
+            deferredInkingShader = Shader.Find("DeferredInking/Line");
+            backPolygonShader = Shader.Find("DeferredInking/InvertedOutline");
+            models = FindObjectsOfType<DeferredInkingModel>();
+
+            foreach (var model in models)
+            {
+                foreach (var mesh in model.meshes)
+                {
+                    var mat = mesh.material;
+                    originalShaders[mat.name] = mat.shader;
+                }
+            }
+        }
+
+        public void OnValueChanged()
+        {
+            int selected = dropdown.value;
+
+            foreach(var model in models)
+            {
+                var meshes = model.meshes;
+                for(int i=0; i < meshes.Count; i++)
+                {
+                    var mat = new Material(meshes[i].material);
+
+                    if (selected == 0)
+                    {
+
+                        mat.shader = originalShaders[mat.name];
+                    }
+                    else if (selected == 1)
+                    {
+                        mat.shader = deferredInkingShader;
+                    }
+                    else
+                    {
+                        mat.shader = backPolygonShader;
+                    }
+
+                    var mesh = meshes[i];
+                    mesh.material = mat;
+                    meshes[i] = mesh;
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
