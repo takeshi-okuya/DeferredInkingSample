dffab76c16636c16f6782ec7f38984d748a563b7
Takeshi Okuya <20336848+takeshi-okuya@users.noreply.github.com>
2020-06-12 01:10:39 +0900

    終了ボタンを作成

---
+++ Assets/_Scenes/scripts/QuitScript.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+namespace WCGL
+{
+    public class QuitScript : MonoBehaviour
+    {
+        public void Quit()
+        {
+#if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+#elif UNITY_STANDALONE
+            UnityEngine.Application.Quit();
+#endif
+        }
+    }
+}
